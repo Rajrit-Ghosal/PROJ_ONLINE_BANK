7bba64282236b0ec57f56441da16fb584bb260be
username <wecp-dummy@gmail.com>
2024-01-28 10:50:13 +0000

    Day_5

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -3,32 +3,100 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Customers;
 import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.service.CustomerServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
-
+@RestController
 public class CustomerController {
-
+    @Autowired
+    private CustomerServiceImpl service;
+    
+    @GetMapping("/customers")
     public ResponseEntity<List<Customers>> getAllCustomers() {
         return null;
     }
 
-    public ResponseEntity<Customers> getCustomerById(int customerId) {
+    @GetMapping("/customers/{customerID}")
+    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerId) {
+        // try {
+        //     return new ResponseEntity<>(service.getCustomerById(customerId),HttpStatus.OK);
+        // } catch (SQLException e) {
+        //     // TODO Auto-generated catch block
+        //     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // }
         return null;
     }
 
-    public ResponseEntity<Integer> addCustomer(Customers customers) {
+    @PostMapping("/customers")
+    public ResponseEntity<Integer> addCustomer(@RequestBody Customers customers) {
+        // try {
+        //     return new ResponseEntity<>(service.addCustomer(customers),HttpStatus.OK);
+        // } catch (SQLException e) {
+        //     // TODO Auto-generated catch block
+        //     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        // }
         return null;
     }
-
+ 
+    @PutMapping("/customers/{customerID}")
     public ResponseEntity<Void> updateCustomer(int customerId, Customers customers) {
+        // try {
+        //     //ResponseEntity rs=new ResponseEntity<>(service.updateCustomer(customers);)
+           
+        
+        // } catch (SQLException e) {
+        //     // TODO Auto-generated catch block
+        //     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // }
         return null;
     }
-    public ResponseEntity<Void> deleteCustomer(int customerId) {
+    @DeleteMapping("/customers/{customerID}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerId) {
         return null;
     }
 
+    @GetMapping("/customers/fromArrayList")
+    public ResponseEntity<List<Customers>> getAllCustomersFromArrayList() {
+        return null;
+    }
+
+    
+    @GetMapping("/customers/toArrayList")
+    public ResponseEntity<List<Customers>> addCustomersToArrayList(Customers customers) {
+        //customersList.add(customers);
+        return null;
+    }
+
+    @GetMapping("/customers/fromArrayList")
+    public  ResponseEntity<List<Customers>> getAllCustomersSortedByNameFromArrayList(){
+        // List<Customers> sortedCustomers = customersList;
+        // Collections.sort(sortedCustomers);
+        // return sortedCustomers;
+        return null;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public ResponseEntity<List<Transactions>> getAllTransactionsByCustomerId(int cutomerId) {
         return null;
     }
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
@@ -10,43 +10,54 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
-    CustomerDAO customerDAO;
+    // @Autowired
+    // CustomerDAO customerDAO;
+    
     private static List<Customers> customersList = new ArrayList<>();
 
-    public CustomerServiceImpl(CustomerDAO customerDAO) {
-        this.customerDAO = customerDAO;
-    }
+    // public CustomerServiceImpl(CustomerDAO customerDAO) {
+    //     //this.customerDAO = customerDAO;
+    // }
 
     @Override
     public List<Customers> getAllCustomers() throws SQLException {
-        return customerDAO.getAllCustomers();
+        //return customerDAO.getAllCustomers();
+        return null;
     }
 
     @Override
     public Customers getCustomerById(int customerId) throws SQLException {
-        return customerDAO.getCustomerById(customerId);
+       // return customerDAO.getCustomerById(customerId);
+       return null;
     }
 
     @Override
     public int addCustomer(Customers customers) throws SQLException {
-        return customerDAO.addCustomer(customers);
+        //return customerDAO.addCustomer(customers);
+        return 0;
     }
 
     @Override
     public void updateCustomer(Customers customers) throws SQLException {
-        customerDAO.updateCustomer(customers);
+        //customerDAO.updateCustomer(customers);
+        
     }
 
     @Override
     public void deleteCustomer(int customerId) throws SQLException {
-        customerDAO.deleteCustomer(customerId);
+        //customerDAO.deleteCustomer(customerId);
+        //return null;
     }
 
     @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        List<Customers> sortedCustomers = customerDAO.getAllCustomers();
+        List<Customers> sortedCustomers = customersList;
         if (sortedCustomers != null) {
             Collections.sort(sortedCustomers);
         }
