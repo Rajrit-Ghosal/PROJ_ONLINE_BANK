62e89be37baa1d66853c1d40adb87cfb57d7f8f0
username <wecp-dummy@gmail.com>
2024-01-28 06:56:50 +0000

    Day_4

--- backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
+++ backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
@@ -1,7 +1,24 @@
 package com.wecp.progressive;
 
+//import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class BankSafeApplication {
     public static void main(String[] args) {
-        System.out.println("Welcome to BankSafe Project!");
+        SpringApplication.run(BankSafeApplication.class, args);
+        System.out.println("Welcome to Banksafe Online!");
+
+
+    }
+
+    @GetMapping("/banksafe")
+    public String msg(){
+        return "Welcome to Banksafe Online!";
     }
 }
