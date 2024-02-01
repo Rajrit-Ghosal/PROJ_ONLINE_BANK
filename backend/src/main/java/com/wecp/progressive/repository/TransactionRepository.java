09dc7debbbd6412f6cb8ad10e83269802c21f6b4
username <wecp-dummy@gmail.com>
2024-02-01 19:00:40 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/TransactionRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TransactionRepository.java
@@ -1,14 +1,13 @@
 package com.wecp.progressive.repository;
 
-import java.util.List;
-
-import javax.transaction.Transaction;
-
+import com.wecp.progressive.entity.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wecp.progressive.entity.Transactions;
+import java.util.List;
+
 @Repository
-public interface TransactionRepository extends JpaRepository<Transactions,Integer> {
-     public List<Transactions> findByCustomerId(int customerId);
+public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
+
+    List<Transactions> findByAccountsAccountId(int accountId);
 }
