7f109186ae0e2e65938c42c90fbc8664470cead8
username <wecp-dummy@gmail.com>
2024-02-11 11:06:48 +0000

    Day 17 not passing

--- frontend/src/app/bank/components/account/account.component.ts
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -1,5 +1,14 @@
 
-export class AccountComponent {
- 
+import { Component } from '@angular/core';
+import { Account } from '../../types/account';
 
+@Component({
+  selector: 'app-accountsample',
+  standalone: true,
+  imports: [],
+  templateUrl: './accountsample.component.html',
+  styleUrls: ['./accountsample.component.css']
+})
+export class AccountsampleComponent {
+   account:Account=new Account("1",500.00,"1");
 }
--- frontend/src/app/bank/components/customer/customer.component.ts
+++ frontend/src/app/bank/components/customer/customer.component.ts
@@ -1,5 +1,17 @@
+import { Component } from '@angular/core';
+//import { Account } from '../../types/Account';
+import { Customer } from '../../types/customer';
 
-export class CustomerComponent  {
 
-  
+@Component({
+  selector: 'app-customersample',
+  standalone: true,
+  imports: [],
+  templateUrl: './customersample.component.html',
+  styleUrls: ['./customersample.component.css']
+})
+export class CustomersampleComponent {
+  // account:Cu = new Account("1",400,"1");
+  // name: string, email: string, username: string, password: string, role: string, customerId?:string)
+   customer:Customer = new Customer("jhon","jhon@gmail.com","jhonuser","User","1");
 }
